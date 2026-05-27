953f38fc8a94da1c7a2e66d959e0ff36f158b387
ahmadbambajabbi1 <ahmadbambajabbi1@gmail.com>
2026-05-27 11:31:59 +0000

    added veriffcation page

--- lib/config/constants.ts
+++ lib/config/constants.ts
@@ -1,5 +1,10 @@
 /** Base URL of admin-service (no trailing slash). */
-export const ADMIN_API_BASE_URL =
-  process.env.NEXT_PUBLIC_ADMIN_API_URL ?? "http://127.0.0.1:5006";
+const rawAdminApiBaseUrl =
+  process.env.NEXT_PUBLIC_ADMIN_API_URL ??
+  "https://paynexa-admin-service-production.up.railway.app";
+
+export const ADMIN_API_BASE_URL = rawAdminApiBaseUrl
+  .replace(/\/$/, "")
+  .replace(/^(?!https?:\/\/)/, "https://");
 
 export const STORAGE_ADMIN_ACCESS_TOKEN = "escrow_admin_access_token";
